46f7008a2156bb131361b9da386482a2b2768307
Praveen3752 <66742627+Praveen3752@users.noreply.github.com>
2021-11-01 09:33:27 +0530

    Delete OOP part 5 directory

--- OOP part 5/src/app.ts
+++
@@ -1,5 +0,0 @@
-import { CurrencyController } from './controllers/currency.controller';
-import { CurrencyService } from './services/currency.service';
-import { CurrencyView } from './views/currency.view';
-
-const app = new CurrencyController(new CurrencyService(), new CurrencyView());
