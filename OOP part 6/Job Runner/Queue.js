e660d7123ee09f271a793382ff033d5c779edf3b
praveen kumar <praveen.indana1995@gmail.com>
2021-11-15 18:10:17 +0530

    resolved comments

--- OOP part 6/Job Runner/Queue.js
+++ OOP part 6/Job Runner/Queue.js
@@ -20,44 +20,73 @@
             if(this.values.length == 0) return this.values;
             let lastIndex = this.values.length-1;
             let parentIndex = Math.floor((lastIndex-1)/2);
-            while(parentIndex >= 0 && this.values[parentIndex].priority > this.values[lastIndex].priority)
+            while(parentIndex >= 0 && this.checkPriorityForParent(parentIndex,lastIndex))
             {
-                [this.values[parentIndex],this.values[lastIndex]] = [this.values[lastIndex],this.values[parentIndex]];
+                this.swapValues(parentIndex,lastIndex);
                 lastIndex = parentIndex;
                 parentIndex = Math.floor((lastIndex-1)/2);
             }
             return this.values;
+        }
+
+        checkPriorityForParent(parentIndex,lastIndex) {
+            return this.values[parentIndex].priority > this.values[lastIndex].priority;
         }
         
         removeValue()
         {
         if(this.values.length == 0) return this.values;
         let lastIndex = this.values.length - 1;
-        [this.values[0],this.values[lastIndex]] = [this.values[lastIndex],this.values[0]];
-        console.log("Removed value");
-        console.log(this.values.pop());
+        this.swapValues(0,lastIndex);
+        this.removeValueandLog();
         let currentIndex = 0;
-        let leftChild = 2*currentIndex + 1;
-        let rightChild = 2*currentIndex + 2;
-        while(leftChild < this.values.length-1 && rightChild < this.values.length-1 && (this.values[leftChild].priority < this.values[currentIndex].priority || this.values[rightChild].priority > this.values[currentIndex].priority))
+        let leftChild = this.getLeftChild(currentIndex);
+        let rightChild = this.getRightChild(currentIndex);
+        while(this.checkForLeftAndRightChild(leftChild,rightChild) && this.checkForPriority(leftChild,rightChild,currentIndex))
         {
                 let temp;
-                if(this.values[leftChild].priority < this.values[rightChild].priority)
-                {
-                    temp = leftChild;
-                }
-                else
-                {
-                    temp = rightChild;
-                }
-                if(leftChild > this.values.length-1) temp = rightChild;
-                if(rightChild > this.values.length-1) temp = leftChild;
-                [this.values[currentIndex],this.values[temp]] = [this.values[temp],this.values[currentIndex]];
+                temp = this.getTempIndex(leftChild,rightChild);
+                this.swapValues(currentIndex,temp);
                 currentIndex = temp;
-                leftChild = 2 * currentIndex + 1;
-                rightChild = 2 * currentIndex + 2;
+                leftChild = this.getLeftChild(currentIndex);
+                rightChild = this.getRightChild(currentIndex);
         }
         return this.values;
         }
+
+        checkForLeftAndRightChild(leftChild,rightChild) {
+            return leftChild < this.values.length-1 && rightChild < this.values.length-1;
+        }
+
+        checkForPriority(leftChild,rightChild,currentIndex) {
+            const leftChildPriority = this.values[leftChild].priority < this.values[currentIndex].priority;
+            const rightChildPriority = this.values[rightChild].priority > this.values[currentIndex].priority;
+            return leftChildPriority || rightChildPriority;
+        }
+
+        getTempIndex(leftChild,rightChild) {
+            let temp;
+            if(leftChild > this.values.length-1) temp = rightChild;
+            if(rightChild >= this.values.length-1) temp = leftChild;
+            return temp;
+        }
+
+        removeValueandLog() {
+        console.log("Removed value");
+        console.log(this.values.pop());
+        }
+
+        getLeftChild(currentIndex) {
+            return 2 * currentIndex + 1;
+        }
+
+        getRightChild(currentIndex) {
+            return 2 * currentIndex + 2;
+        }
+
+        swapValues(firstIndex,secondIndex) {
+            [this.values[firstIndex],this.values[secondIndex]] = [this.values[secondIndex],this.values[firstIndex]];
+        }
+
 
     }
